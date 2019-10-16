45ba4097b600c67d075e8801bca4e614709e1765
d3m02 <d3m02@DESKTOP.localdomain>
2019-10-17 01:06:29 +0300

    reorganize project, experemental in new brunch

--- product.cpp
+++
@@ -1,9 +0,0 @@
-#include "product.h"
-void product::send_human()
-{
-    cout<<sc_time_stamp().to_string()<<"\t"<<"Transport got human!"<<endl;
-}
-
-SC_CTOR(product){
-    cout<<"product was created!"<<endl
-};
--- ship.cpp
+++
@@ -1,9 +0,0 @@
-#include "ship.h"
-void ship::send_human()
-{
-    cout<<sc_time_stamp().to_string()<<"\t"<<"Transport got human!"<<endl;
-}
-
-SC_CTOR(ship){
-    cout<<"Ship was created!"<<endl
-};
